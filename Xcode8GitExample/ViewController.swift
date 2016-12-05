62b1794c1a92cf5d6e310d7eb0a06f63cdbe407f
Ben Austin <benaustin@Bens-MacBook.local>
2016-12-05 14:16:14 -0700

    Initial Commit

---
+++ Xcode8GitExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode8GitExample
+//
+//  Created by Ben Austin on 12/5/16.
+//  Copyright © 2016 Ben Austin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
